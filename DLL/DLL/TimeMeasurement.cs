f9fb95b455e71750fdd5f06ccc820b0e8a9df748
Micky <Micky@LAPTOP-R1NRCFQA>
2017-02-19 16:05:42 +0100

    Timing class complete

--- DLL/DLL/TimeMeasurement.cs
+++ DLL/DLL/TimeMeasurement.cs
@@ -1,16 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 
 namespace DLL
 {
     class TimeMeasurement
     {
-        public void SomeFunction()
-        {
+        private long start;
+        private long stop;
+        private long frequency;
+        Decimal multiplier = new Decimal(1.0e9);
 
+        // Importing important DLL's that will be used
+        [DllImport("KERNEL32")]
+        private static extern bool QueryPerformanceCounter(out long lpPerformanceCount);
+
+        [DllImport("Kernel32.dll")]
+        private static extern bool QueryPerformanceFrequency(out long lpFrequency);
+
+        // Timing constructor
+        public TimeMeasurement()
+        {
+            if (QueryPerformanceFrequency(out frequency) == false)
+            {
+                // Frequency not supported
+                throw new Win32Exception();
+            }
+        }
+
+        // Method for starting the counter
+        public void Start()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            QueryPerformanceCounter(out start);
+        }
+
+        // Method for stopping the counter
+        public void Stop()
+        {
+            QueryPerformanceCounter(out stop);
+        }
+
+        // Returns the average time between iterations in nanoseconds
+        public double Duration(int iterations)
+        {
+            return ((((double)(stop - start) * (double)multiplier) / (double)frequency) / iterations);
+        }
+
+        // Get the elapsed time between the start and the stop function in nanoseconds
+        public double getElapsedTime()
+        {
+            //return ((TimeSpan)(dtEndTime - dtStartTime)).TotalMilliseconds;
+            return (((double)(stop - start) * (double)multiplier) / (double)frequency);
         }
     }
 }
