f3e6d48ccad4c443b2f09dab6d916d464978971b
Codrut Marin <codrut.marin@student.stenden.com>
2017-03-06 00:28:39 +0100

    Stones MinMax Sequential Search BinarySearch

---
+++ DLL/DLL/BinarySearch.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    class BinarySearch
+    {
+        /// <summary>
+        /// Input: the array needs to be already sorted 
+        /// Unsing a recursive function is searching for the element 
+        /// </summary>
+        /// <typeparam name="T">Generic type</typeparam>
+        /// <param name="array">Array given by the user/program</param>
+        /// <param name="element">The element to be found</param>
+        /// <param name="start">Where the search starts</param>
+        /// <param name="end">Where the search ends</param>
+        /// <returns></returns>
+        private Boolean Search<T>(T[] array, T element, double start, double end) where T : IComparable
+        {
+            // If the starting point is bigger then the ending point or the ending point is smaller then the starting point
+            // There is no element to be found
+            if (start <= end)
+            {
+                int m = (int)Math.Round((start + end) / 2); // Here is calculated the middle index of the array 
+                if (array[m].Equals(element)) //If the middle element of the array is the element that we are searching for then we found it
+                {
+                    return true;
+                }
+                else if (array[m].CompareTo(element) < 0) //If not then is checking if the element is on the right side of the array 
+                {
+                    start = (double)(m + 1);
+                    return Search(array, element, start, end); // splits the array in half and searches in the right part
+                }
+                else //if not 
+                {
+                    end = (double)(m - 1);
+                    return Search(array, element, start, end); // splits the array in half and searches in the left part 
+                }
+            }
+            else // if there is no element found display
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        ///  This function is used for getting the array from the main program
+        ///  Here is calculated the size of the array and passed forward to the function that does all the work Search
+        /// </summary>
+
+        public Boolean binarySearch<T>(T[] array, T element) where T : IComparable
+        {
+            double size = array.Length - 1;
+            return Search(array, element, 0, size);
+        }
+    }
+}
---
+++ DLL/DLL/SequentialSearch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    class SequentialSearch
+    {
+        /// <summary>
+        ///  Sequnetial Search is an algorithm that search element by element in an array
+        ///  till finds the desired element
+        /// </summary>
+        /// <typeparam name="T"> Generic type</typeparam>
+        /// <param name="array"> Array given by user/program</param>
+        /// <param name="element"> The element to be found</param>
+        /// <returns></returns>
+        public Boolean sequentialSearch<T>(T[] array, T element) where T : IComparable
+        {
+            Boolean sw = true; // switch to stop the loop from running 
+            int size = array.Length;  // geting the size of the array
+            for (int i = 0; i < size && sw; i++) //looping through the array
+            {
+                if (array[i].Equals(element)) //checking if is the element 
+                {
+                    sw = false; // if yes then stop the loop
+                    return true; // return true
+                }
+            }
+            return false; // after finishing the loop the element wasn't detected return false
+        }
+    }
+}
