a29c77e7b0252f4f7bfc2f9c25ce759aa6fcabd8
Eric <smith.eric.george@gmail.com>
2017-03-06 12:14:44 +0100

    Add generic LinkedList

---
+++ DLL/DLL/LinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    class Node<K,T>
+    {
+        public K Key;
+        public T Item;
+        public Node<K, T> NextNode;
+
+        public Node()
+        {
+            Key = default(K);
+            Item = default(T);
+            NextNode = null;
+        }
+
+        public Node(K key, T item, Node<K,T> nextNode)
+        {
+            Key = key;
+            Item = item;
+            NextNode = nextNode; 
+        }
+    }
+    public class LinkedList<K,T>
+    {
+        Node<K, T> m_Head;
+        public LinkedList()
+        {
+            m_Head = new Node<K, T>();
+        }
+        public void AddHead(K key,T item)
+        {
+            Node<K, T> newNode = new Node<K, T>(key,item,m_Head.NextNode);
+            m_Head.NextNode = newNode;
+        }
+
+    }
+}
