4da0e1564d8041e50ca1fee19326dfc6f1b9018a
Codrut Marin <codrut.marin@student.stenden.com>
2017-03-13 18:26:34 +0100

    Some other changes

--- DLL/DLL/BucketHashing.cs
+++ DLL/DLL/BucketHashing.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DLL;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,6 +8,62 @@ using System.Threading.Tasks;
 
 namespace DLL
 {
+    public struct Slot<T>: IComparable where T : IComparable {
+        public T key;
+        public T value;
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+                return 1;
+            if (obj.GetType().Equals(this.GetType()))
+            {
+                Slot<T> other = (Slot<T>)obj;
+                return this.key.CompareTo(other.key);
+            }
+            else
+                throw new ArgumentException("Object is not an Item");
+        }
+    }
+
+    class HashTable<T> where T: IComparable
+    {
+        private GArrayList<Slot<T>> hashTable;
+        private Slot<T> element;
+        private int size;
+
+        public HashTable()
+        {
+            hashTable = new GArrayList<Slot<T>>();
+            size = 0;
+        }
+
+        public void Add(T key, T value)
+        {
+            element = new Slot<T>();
+            element.key = key;
+            element.value = value;
+            hashTable.Add(element);
+            size++;
+        }
+
+        public void Clear()
+        {
+            if(size > 0)
+            {
+                for(int i = size - 1; i >= 0; i--)
+                {
+                    hashTable.Remove(i);
+                }
+                size = 0;
+            }
+        }
+
+        public void ContainsKey()
+        {
+
+        }
+    }
     class BucketHashing <T>
     {
         private const int size = 101;
