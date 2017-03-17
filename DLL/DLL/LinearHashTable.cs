603ad6601341dd4b907e81871184645a76e5f0b7
Micky <Micky@LAPTOP-R1NRCFQA>
2017-03-17 09:59:07 +0100

    Definitive version of LinearHash

--- DLL/DLL/LinearHashTable.cs
+++ DLL/DLL/LinearHashTable.cs
@@ -6,7 +6,7 @@ using System.Reflection;
 namespace LinearHashTable
 {
     public class LinearHashTable<T> : IEnumerable
-    {   
+    {
         private T[] list;
         private int percentageBorder;
 
@@ -227,4 +227,3 @@ namespace LinearHashTable
         }
     }
 }
-
