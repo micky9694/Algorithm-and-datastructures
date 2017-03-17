2608677189045d030e5fafa776e40d62f15e30de
user120310 <user120310@hotmail.com>
2017-03-17 10:34:07 +0100

    Change again !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ DLL/DLL/Games.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLL
+{
+    class Games
+    {
+        private double price;
+        private String name;
+
+        public Games()
+        {
+
+        }
+
+        public Games(String gamename, double gamePrice)
+        {
+            name = gamename;
+            price = gamePrice;
+        }
+
+        public String GameName
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public double GamePrice
+        {
+            get { return this.price; }
+            set { this.price = value; }
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+            {
+                return 1;
+            }
+
+            Consoles other = obj as Consoles;
+            if (other != null)
+                return this.consoleName.CompareTo(other.consoleName);
+            else
+                throw new ArgumentException("Object is not a Console");
+        }
+    }
+}
